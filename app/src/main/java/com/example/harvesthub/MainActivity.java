79a6f248fc942047b514c2ebf26ed4a135f3c3d6
JaidynGM <jaidynrocks01@gmail.com>
2023-11-30 10:04:56 -0800

    Added functionality for email and password, they have to have some text to login

--- app/src/main/java/com/example/harvesthub/MainActivity.java
+++ app/src/main/java/com/example/harvesthub/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +17,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void login (View view){
+        //Create intent for going to different pages
         Intent intent = new Intent(this, Homepage.class);
-        startActivity(intent);
+
+        //Toast variables
+        int duration = Toast.LENGTH_SHORT;
+        CharSequence text = "";
+
+        //get email to string
+        EditText email = findViewById(R.id.emailText);
+        String Email = email.getText().toString();
+
+        //get password to string
+        EditText password = findViewById(R.id.passwordText);
+        String passWord = password.getText().toString();
+
+        //Check to see if a username or password has been typed
+        if (Email.isEmpty()){
+            text = "Please enter your email";
+            Toast toast = Toast.makeText(this, text, duration);
+            toast.show();
+        } else if (passWord.isEmpty()){
+            text = "Please enter your password";
+            Toast toast = Toast.makeText(this, text, duration);
+            toast.show();
+        }else{
+            startActivity(intent);
+        }
     }
 }
