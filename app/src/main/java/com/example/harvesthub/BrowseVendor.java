c4014bb73189a3c3bd7e800685feb2eb7ca4a216
JaidynGM <jaidynrocks01@gmail.com>
2023-11-30 16:04:26 -0800

    Added spinner and xml file for spinner

--- app/src/main/java/com/example/harvesthub/BrowseVendor.java
+++ app/src/main/java/com/example/harvesthub/BrowseVendor.java
@@ -3,12 +3,29 @@ package com.example.harvesthub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class BrowseVendor extends AppCompatActivity {
+
+    Spinner vendorName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse_vendor);
+
+        //initialize spinner
+        vendorName = (Spinner) findViewById(R.id.spinner);
+
+        //getting the information for spinners
+        ArrayAdapter<CharSequence> venName = ArrayAdapter.createFromResource(this, R.array.vendorName,
+                android.R.layout.simple_spinner_item);
+
+        //allow for the spinners to drop down
+        venName.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //set the adapters
+        vendorName.setAdapter(venName);
     }
 }
