19757d437122894734ed7b6dbfe4fd61324847eb
Jaidyn Gordon-Mason <jaidynrocks01@gmail.com>
2023-12-04 17:06:04 -0800

    Fixed manifest file

---
+++ app/src/main/java/com/example/harvesthub/checklist.java
@@ -0,0 +1,14 @@
+package com.example.harvesthub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class checklist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checklist);
+    }
+}
